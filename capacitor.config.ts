8b79f7f44db7c359549381006cc15ab8642803b3
Edd Clarke <edward.earl.clarke@gmail.com>
2023-02-26 00:41:21 -0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-vue-eslint-prettier-starter',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
